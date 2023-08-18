f333a70f8171297e80d626344276eae1e69c8f18
Chinthan <Chinthanshetty2005@gmail.com>
2023-08-18 12:41:30 +0530

    Division functon added to arith.py

--- python_progs/arith.py.py
+++ python_progs/arith.py.py
@@ -7,8 +7,13 @@ def sub (n1,n2):
 def mul (n1,n2):
     return(n1*n2)
 
+def div (n1,n2):
+    return(n1/n2)
+
 a=int(input("Enter a num1:"))
 b=int(input("Enter a num2:"))
 print("Sum=",add(a,b))
 print("Difference=",sub(a,b))
 print("Product=",mul(a,b))
+print("difference=",div(a,b))
+
